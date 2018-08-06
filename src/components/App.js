bda64ca8b5e90769e41bd01f6df7b6920164d564
sandiasolutions <danieleaton747@gmail.com>
2018-08-06 13:56:37 -0600

    began navigation bar for sconsdoc

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React,{Component} from "react";
 import Typography from '@material-ui/core/Typography';
 import NavigationBar from "./NavigationBar";
+import {theme} from "./UNMTheme";
+import {MuiThemeProvider} from "@material-ui/core"
+
 
 export default class App extends Component{
     render(){
         return (
             <div>
-                <NavigationBar/>
-                <Typography variant="display4" gutterBottom>
-                    Display 4
-                </Typography>
+                <MuiThemeProvider theme={theme}>
+                    <NavigationBar color={'primary'}/>
+                    <Typography variant="display4" gutterBottom>
+                        Display 4
+                    </Typography>
+                </MuiThemeProvider>
             </div>
 
     )
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,9 +3,12 @@ import {AppBar} from "@material-ui/core";
 import {Toolbar} from "@material-ui/core";
 import {Typography} from "@material-ui/core";
 import {Button} from "@material-ui/core";
+import {Menu} from "@material-ui/core";
+import {IconButton} from "@material-ui/core";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
 
 export default class NavigationBar extends Component{
-
 
 
     constructor(props){
@@ -15,16 +18,15 @@ export default class NavigationBar extends Component{
 
     render(){
         return (
-            <AppBar position="static" color={"#2196f3"}>
-                <Toolbar  color={"#2196f3"}>
-                    {/*<IconButton color="inherit" aria-label="Menu">*/}
-                    {/*</IconButton>*/}
-                    <Typography variant="title" color="inherit">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+            <AppBar position="static" color={"primary"}>
+                <Toolbar>
+                    <Button color="inherit">Menu 1</Button>
+                    <Button color="inherit">Menu 2</Button>
+                    <Button color="inherit">Menu 3</Button>
+                    <Button color="inherit">Menu 4</Button>
+                    <Button color="inherit">Menu 5</Button>
                 </Toolbar>
-            </AppBar>        );
+            </AppBar>);
     }
 
 }
