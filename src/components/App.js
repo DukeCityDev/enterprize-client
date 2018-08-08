f60ac11f3112806adf54cbd490d4adcb5d720b91
sandiasolutions <danieleaton747@gmail.com>
2018-08-08 14:54:08 -0600

    began sideMenu styling and menu population

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,6 @@ export default class App extends Component{
                 </MuiThemeProvider>
             </div>
 
-    )
+        )
     };
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,31 +1,89 @@
 import React,{Component} from "react";
-import {AppBar} from "@material-ui/core";
-import {Toolbar} from "@material-ui/core";
-import {Typography} from "@material-ui/core";
-import {Button} from "@material-ui/core";
-import {Menu} from "@material-ui/core";
-import {IconButton} from "@material-ui/core";
+import {ListItemText,ListItem,IconButton,Toolbar,Button,AppBar,Drawer,List,Divider} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
 
 export default class NavigationBar extends Component{
 
-
     constructor(props){
         super(props);
+
+        this.state = {
+          sideMenuOpened: false
+        };
     }
 
+    styles = {
+        drawer:{
+            width : "30vw",
+        },
+        listItem:{
+            textAlign:"center"
+        }
+    };
+
+    sideMenuOpen = ()=>{
+        console.log("menu opened");
+        this.setState({sideMenuOpened: true});
+    };
+
+    sideMenuClose = ()=>{
+        console.log("menu closed");
+        this.setState({sideMenuOpened: false});
+    };
 
     render(){
+        const {sideMenuOpened} = this.state;
+        let sideMenuOpen = this.sideMenuOpen.bind(this);
+        let sideMenuClose = this.sideMenuClose.bind(this);
         return (
-            <AppBar position="static" color={"primary"}>
+            <AppBar position={"static"} color={"primary"}>
                 <Toolbar>
-                    <Button color="inherit">Menu 1</Button>
-                    <Button color="inherit">Menu 2</Button>
-                    <Button color="inherit">Menu 3</Button>
-                    <Button color="inherit">Menu 4</Button>
-                    <Button color="inherit">Menu 5</Button>
+                    <IconButton color="inherit" aria-label="Menu">
+                        <MenuIcon onClick={sideMenuOpen} />
+                    </IconButton>
                 </Toolbar>
+                <Drawer open={sideMenuOpened} onClose={sideMenuClose}>
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        onClick={sideMenuClose}
+                        onKeyDown={sideMenuClose}
+                    >
+                        <div style={this.styles.drawer}>
+                            <List>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem} />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem} />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText inset primary="Sent mail" style={this.styles.listItem}/>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </div>
+                </Drawer>
             </AppBar>);
     }
 
